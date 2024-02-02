e3138e5ae0f5da9b812153d80ccf13343e4e5669
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-02-02 17:09:38 +0600

    updated code

--- For Practice/JavaScript/evenNumber.js
+++ For Practice/JavaScript/evenNumber.js
@@ -3,10 +3,12 @@
 // Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
 
 let numbers = [12, 98, 5, 41, 23, 78, 46];
-
+ let store = [];
 for(let i = 0; i< numbers.length; i++){
     let num = numbers[i];
     if(num % 2 === 0){
-    
+        store.push(num);
     }
 }
+console.log(store);
+
